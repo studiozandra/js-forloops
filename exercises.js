5f8f2495508b7c25cca76ed3ebca4a49d0be9012
studiozandra <zan@studiozandra.com>
2019-01-20 20:23:41 -1000

    add function 10 Copy Pasta

--- exercises.js
+++ exercises.js
@@ -191,7 +191,15 @@ Write a function named `copyArray` which takes two arguments: `originArray` and 
 Inside of this function write a for-loop that will iterate through the contents of the `originArray` and pushes each element of that array into `destinationArray`. Console.log your result.
 */
 
-
+var valuesArray = [99, 66, 829, 1941, 8, 76];
+var copyValuesArray = [];
+function copyArray(originArray, destinationArray){
+   for (i = 0; i < originArray.length; i++){
+      destinationArray.push(originArray[i]);
+   }
+   return destinationArray;
+}
+console.log(copyArray(valuesArray, copyValuesArray));
 
 /*Final Boss*/
 
